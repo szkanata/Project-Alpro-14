5387725756ab42785b485e9ad9ccef9c2c0cc42e
szkanata <ninorin0001@gmail.com>
2026-06-13 15:32:25 +0800

    first commit

---
+++ umum.cpp
@@ -0,0 +1,28 @@
+// #include <iostream>
+// #include <conio.h>
+// using namespace std;
+
+// struct pengguna {
+//     string nama;
+//     int pin;
+//     string username;
+//     string password;
+// }
+
+// struct akun {
+//     pengguna* activeakun;
+//     pengguna* next;
+//     pengguna data;
+// }
+
+// akun* head = nullptr;
+
+// void signup {
+//     akun* newakun = new akun;
+//     cout << 
+// }
+
+// int main () {
+
+//     return 0;
+// }
